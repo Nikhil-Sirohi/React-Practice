591b34f5439e372baeef614cf93998d540009ead
Nikhil Sirohi <nikhlsirohy001@gmail.com>
2025-03-31 05:49:53 +0530

    Added simple-api.js with user data fetching functionality

---
+++ simple-api.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+export default function FetchUsers() {
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        if (!response.ok) {
+          throw new Error("failed to fetch users");
+        }
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchUsers();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <div>
+      <h1>Users list :-</h1>
+      <ul>
+        {users.map((user) => {
+          <li key={user.id}>{user.name}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
